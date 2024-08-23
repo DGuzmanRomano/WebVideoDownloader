b0481e6d73ab70cb23358c228e94ae655d86bbf9
DGuzmanRomano <131337401+DGuzmanRomano@users.noreply.github.com>
2024-08-22 19:03:02 -0600

    Change to yt_dlp

--- main.py
+++ main.py
@@ -1,62 +1,53 @@
 from tkinter import *
 from tkinter import filedialog
-from pytube import YouTube
-from moviepy.editor import *
-
+import yt_dlp
+import shutil
 
 def download():
-    video_path= url_entry.get()
-    file_path =  path_label.cget("text")
-    mp4 = YouTube(video_path).streams.get_highest_resolution().download()
-    video_clip = VideoFileClip(mp4)
-    video_clip.close()
+    video_url = url_entry.get()
+    download_path = path_label.cget("text")
+    print('Downloading...')
 
+    ydl_opts = {
+        'format': 'best',
+        'outtmpl': 'downloaded_video.%(ext)s',
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info_dict = ydl.extract_info(video_url, download=True)
+        video_file = ydl.prepare_filename(info_dict)
+
+    shutil.move(video_file, download_path)
+    print('Download Complete')
 
 def get_path():
-    path= filedialog.askdirectory()
+    path = filedialog.askdirectory()
     path_label.config(text=path)
 
 root = Tk()
-root.title('Video downloader')
+root.title('Video Downloader')
 
-canvas = Canvas(root,width=400, height = 300)
+canvas = Canvas(root, width=400, height=300)
 canvas.pack()
 
+# App label
+app_label = Label(root, text="Video Downloader", fg="blue", font=('Arial'))
+canvas.create_window(200, 20, window=app_label)
 
-
-#app label
-app_label = Label (root, text = "Video downloader", fg = "blue", font= ('Arial'))
-
-
-
-canvas.create_window(200,20,window=app_label)
-
-
-#entry to accpet URL
-
+# URL entry
 url_entry = Entry(root)
-url_label= Label(root, text="Enter video URL")
+url_label = Label(root, text="Enter video URL")
+canvas.create_window(200, 80, window=url_label)
+canvas.create_window(200, 100, window=url_entry)
 
+# Path selection
+path_label = Label(root, text="Select path to download")
+path_button = Button(root, text="Select", command=get_path)
+canvas.create_window(200, 150, window=path_label)
+canvas.create_window(200, 170, window=path_button)
 
-canvas.create_window(200,80,window=url_label)
-
-canvas.create_window(200,100,window=url_entry)
-
-#path to download
-
-path_label = Label(root,text="Select path to download")
-path_button = Button(root,text="Select", command=get_path)
-
-
-canvas.create_window(200,150,window=path_label)
-
-canvas.create_window(200,170,window=path_button)
-
-
-#Download Button
+# Download button
 download_button = Button(text='Download', command=download)
-
-canvas.create_window(200,250,window=download_button)
-
+canvas.create_window(200, 250, window=download_button)
 
 root.mainloop()
